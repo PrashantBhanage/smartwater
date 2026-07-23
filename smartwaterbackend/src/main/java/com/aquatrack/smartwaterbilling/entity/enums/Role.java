04b52b7546565fe1413a9caaae1269a41e619de5
Prashant Bhanage <prashantbhanage717@gmail.com>
2026-07-23 11:13:45 +0530

    Add smartwaterbackend/src/main/java/com/aquatrack/smartwaterbilling/entity/enums/Role.java

---
+++ smartwaterbackend/src/main/java/com/aquatrack/smartwaterbilling/entity/enums/Role.java
@@ -0,0 +1,11 @@
+package com.aquatrack.smartwaterbilling.entity.enums;
+
+/**
+ * User roles in the SmartWater system.
+ * ADMIN - apartment complex administrator with full access.
+ * RESIDENT - tenant linked to a specific household.
+ */
+public enum Role {
+    ADMIN,
+    RESIDENT
+}
